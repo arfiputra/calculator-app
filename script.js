76b0be25eb128daac9885d8e2f1f1f18f0df1822
unknown <arfiputrapratama21@gmail.com>
2025-10-01 13:04:24 +0700

    Membuat calculator 3D yang sederhana

---
+++ script.js
@@ -0,0 +1,28 @@
+let display = document.getElementById("display");
+let toggleBtn = document.querySelector(".toggle-btn");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    toggleBtn.innerText = "☀️ Light Mode";
+  } else {
+    toggleBtn.innerText = "🌙 Dark Mode";
+  }
+}
